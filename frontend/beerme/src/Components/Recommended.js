f54c0b547d4428fcb2b87a59aa10737992de958e
Marine Cossoul <mcossoul@calpoly.edu>
2020-02-20 13:43:42 -0800

    basic recommended skeleton

--- frontend/beerme/src/Components/Recommended.js
+++ frontend/beerme/src/Components/Recommended.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import NetClient from "../Utils/NetClient";
 
 export class Recommended extends Component {
   constructor(props) {
@@ -6,12 +7,40 @@ export class Recommended extends Component {
     this.state = {};
   }
 
-  render() {
+  componentDidMount() {
+    NetClient.get("http://jsonplaceholder.typicode.com/photos").then(data => {
+      this.setState({ photos : data.slice(0, 10) });
+      localStorage.setItem("appState", data.slice(0, 10));
+    });
+  }
+
+  renderBody() {
     return (
-      <div>
-        <h1>Recommended</h1>
+      <div style={styles.inColumnStyle}>
+        <h2>Recommendations:</h2>
+        {this.state.photos.map(beer => (
+          <p key={beer.id}>{beer.title}</p>
+        ))}
       </div>
     );
+  }
+
+  render() {
+    return (
+      <div style = {styles.inColStyle}>
+        <h1>Recommended For You</h1>
+        <h5>Here is our personalized recommendation for new beer styles based off people with similar tastes!</h5>
+        {this.state.photos ? this.renderBody() : null}
+      </div>
+    );
+  }
+}
+const styles = {
+  inColStyle: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    marginLeft: "200px",
   }
 }
 
