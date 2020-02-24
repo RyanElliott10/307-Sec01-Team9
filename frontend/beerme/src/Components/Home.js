86cc2b8da20849721bc7a8ffdebe83e05458479f
marinecossoul <marine.cossoul@gmail.com>
2020-02-23 16:23:35 -0800

    Home search bar (#69)
    * basic logo codee
    
    * basic recommended skeleton
    
    * font change to top 10 beers
    
    * install npm search box
    
    * added search bar, adjusted styling. Search bar currently connected to dummy data until we can connect wit back end

--- frontend/beerme/src/Components/Home.js
+++ frontend/beerme/src/Components/Home.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-
 import BOTD_Photo from "../img/BOTD_photo.png";
 import Logo from "../img/BeerMe_Logo.png";
 import NetClient from "../Utils/NetClient";
 import Separator from "../img/Sep_Img.png";
+import ReactSearchBox from 'react-search-box'
 
 export class Home extends Component {
   constructor(props) {
@@ -23,7 +23,7 @@ export class Home extends Component {
       <div style={styles.inRowStyle}>
         {this.renderBOTD()}
         <img src={Separator} alt="Separator" />
-        <div style={styles.inColumnStyle}>
+        <div style={styles.inListStyle}>
           <h2>Top Ten Beer Styles</h2>
           {this.state.topTen.map(beer => (
             <p key={beer.id}>{beer.title}</p>
@@ -36,9 +36,9 @@ export class Home extends Component {
   renderBOTD() {
     return (
       <div style={styles.inRowStyle}>
-        <img src={BOTD_Photo} alt="BOTD_Photo" />
+        <img src={BOTD_Photo} alt="BOTD_Photo" style={{width: 150, height:200}}/>
         <div style={styles.inColumnStyle}>
-          <h2>Beer Of The Day</h2>
+          <h2>Beer Of The Day</h2>  
           <p>This is where the Beer of The Day explanation lives.</p>
           <p>More information about the Beer of The Day here.</p>
           <p>And we continue to talk about the Beer of the Day.</p>
@@ -56,6 +56,41 @@ export class Home extends Component {
     );
   }
 
+  data = [
+    {
+      key: 'Corona',
+      value: 'Corona',
+    },
+    {
+      key: 'Modelo',
+      value: 'Modelo',
+    },
+    {
+      key: 'Lagunitas',
+      value: 'Lagunitas',
+    },
+    {
+      key: 'Blue Moon',
+      value: 'Blue Moon',
+    },
+    {
+      key: '805',
+      value: '805',
+    },
+  ]
+
+  renderSearchBox() {
+    return (
+      <div style= {{width: "50%", marginRight: "auto", marginLeft: "auto", marginBottom: "40px"}}>
+        <ReactSearchBox
+          placeholder="Search"
+          data={this.data}
+          callback={record => console.log(record)}
+        />
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
@@ -64,10 +99,11 @@ export class Home extends Component {
             marginLeft: "200px",
             marginRight: "200px",
             marginTop: "45px",
-            alignContent: "center"
+            alignContent: "center",
           }}
         >
           {this.renderLogo()}
+          {this.renderSearchBox()}
           {this.state.topTen ? this.renderBody() : null}
         </div>
       </div>
@@ -79,19 +115,24 @@ const styles = {
   logoStyle: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "center"
+    alignItems: "center",
   },
   inRowStyle: {
     marginRight: "20px",
     marginLeft: "20px",
     display: "flex",
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "top",
   },
   inColumnStyle: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "center"
+    alignItems: "center",
+  },
+  inListStyle: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "right",
   }
 };
 
