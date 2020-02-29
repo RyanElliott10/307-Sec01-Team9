a76c90bdf29c024425a78d7ded489e0db9c1db30
Ryan Elliott <ryelliot@calpoly.edu>
2020-02-28 19:43:50 -0800

    Added Account page details

--- frontend/beerme/src/Components/Account.js
+++ frontend/beerme/src/Components/Account.js
@@ -1,20 +1,136 @@
 import React, { Component } from "react";
-// import { Form, Button, ButtonToolbar, Row, Col } from "react-bootstrap";
-// import * as Constants from "../Utils/Constants";
+import { Button, ButtonToolbar, Form, Col } from "react-bootstrap";
+import MainController from "../Controllers/UserController";
+
+// TODO: Add onAddBeerClick() that, among other things, prints the current state.
 
 export class Account extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      addBeerData: {
+        name: "",
+        styles: "",
+        color: ""
+      }
+    };
+  }
+
+  _validateLoginForm() {
+    return true;
+    // return (
+    //   this.state.signInData.email.length > 0 &&
+    //   this.state.signInData.password.length > 0
+    // );
+  }
+
+  _renderControl(type, value, isDisabled, onChange = () => {}) {
+    return (
+      <Form.Control
+        disabled={isDisabled}
+        type={type}
+        placeholder={value}
+        onChange={onChange}
+      />
+    );
+  }
+
+  _renderCommon() {
+    return (
+      <Form.Group as={Col} controlId="formGridFirstName">
+        <Form onSubmit={this._handleCreateAccountSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridFirstName">
+              <Form.Label>First Name</Form.Label>
+              {this._renderControl("name", "Mark", true)}
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridLastName">
+              <Form.Label>Last Name</Form.Label>
+              {this._renderControl("name", "Johnson", true)}
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Email Address</Form.Label>
+              {this._renderControl("email", "Email", true)}
+            </Form.Group>
+          </Form.Row>
+        </Form>
+      </Form.Group>
+    );
+  }
+
+  _renderAddBeer() {
+    if (!MainController.isBusiness()) {
+      return null;
+    }
+    return (
+      <Form.Group as={Col} controlId="formGridFirstName">
+        <Form onSubmit={this._handleCreateAccountSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridFirstName">
+              <Form.Label>Name of Beer</Form.Label>
+              {this._renderControl("name", "Corona", false, e => {
+                const newBeerData = this.state.addBeerData;
+                newBeerData.name = e.target.value;
+                this.setState({
+                  addBeerData: newBeerData
+                });
+              })}
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridLastName">
+              <Form.Label>Style(s)</Form.Label>
+              {this._renderControl("name", "IPA", false, e => {
+                const newBeerData = this.state.addBeerData;
+                newBeerData.styles = e.target.value;
+                this.setState({
+                  addBeerData: newBeerData
+                });
+              })}
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Color</Form.Label>
+              {this._renderControl("name", "Dark", false, e => {
+                const newBeerData = this.state.addBeerData;
+                newBeerData.colors = e.target.value;
+                this.setState({
+                  addBeerData: newBeerData
+                });
+              })}
+            </Form.Group>
+          </Form.Row>
+          <ButtonToolbar style={{ justifyContent: "space-between" }}>
+            <Button disabled={!this._validateLoginForm()} type="submit">
+              Add a Beer
+            </Button>
+          </ButtonToolbar>
+        </Form>
+      </Form.Group>
+    );
   }
 
   render() {
     return (
-      <div>
-        <h1>Account</h1>
+      <div className="Login" style={broadStyle}>
+        <Form.Row>
+          {this._renderCommon()}
+          {this._renderAddBeer()}
+        </Form.Row>
       </div>
     );
   }
 }
+
+const broadStyle = {
+  paddingTop: "50px",
+  marginLeft: "100px",
+  marginRight: "100px",
+  alignItems: "center",
+  justifyContent: "center"
+};
 
 export default Account;
--- frontend/beerme/src/Components/Home.js
+++ frontend/beerme/src/Components/Home.js
@@ -3,7 +3,7 @@ import BOTD_Photo from "../img/BOTD_photo.png";
 import Logo from "../img/BeerMe_Logo.png";
 import NetClient from "../Utils/NetClient";
 import Separator from "../img/Sep_Img.png";
-import ReactSearchBox from 'react-search-box'
+import ReactSearchBox from "react-search-box";
 
 export class Home extends Component {
   constructor(props) {
@@ -21,18 +21,22 @@ export class Home extends Component {
   renderTopTen() {
     return (
       <div style={styles.inListStyle}>
-          <h2>Top Ten Beer Styles</h2>
-          {this.state.topTen.map(beer => (
-            <p key={beer.id}>{beer.title}</p>
-          ))}
+        <h2>Top Ten Beer Styles</h2>
+        {this.state.topTen.map(beer => (
+          <p key={beer.id}>{beer.title}</p>
+        ))}
       </div>
-    )
+    );
   }
 
   renderBody() {
     return (
       <div style={styles.inRowStyle}>
-        <img src={BOTD_Photo} alt="BOTD_Photo" style={{ width: 110, height: 180, marginRight: "20px" }} />
+        <img
+          src={BOTD_Photo}
+          alt="BOTD_Photo"
+          style={{ width: 110, height: 180, marginRight: "20px" }}
+        />
         {this.renderBOTD()}
         <img src={Separator} alt="Separator" />
         {this.renderTopTen()}
@@ -41,10 +45,16 @@ export class Home extends Component {
   }
 
   renderBOTD() {
-    return (         
+    return (
       <div style={styles.inColumnStyle}>
         <h2>Beer Of The Day</h2>
-        <p>Hive 56 is a dark sour ale aged with honey in an oak foudre for eighteen months. The beer is deep mahogany in color with aromas of strawberries, lemon, and tropical fruit; notes of dark chocolate, raspberries, and figs present themselves upon the first sip, followed by a lingering tartness.</p>
+        <p>
+          Hive 56 is a dark sour ale aged with honey in an oak foudre for
+          eighteen months. The beer is deep mahogany in color with aromas of
+          strawberries, lemon, and tropical fruit; notes of dark chocolate,
+          raspberries, and figs present themselves upon the first sip, followed
+          by a lingering tartness.
+        </p>
       </div>
     );
   }
@@ -59,37 +69,44 @@ export class Home extends Component {
 
   data = [
     {
-      key: 'Corona',
-      value: 'Corona',
+      key: "Corona",
+      value: "Corona"
     },
     {
-      key: 'Modelo',
-      value: 'Modelo',
+      key: "Modelo",
+      value: "Modelo"
     },
     {
-      key: 'Lagunitas',
-      value: 'Lagunitas',
+      key: "Lagunitas",
+      value: "Lagunitas"
     },
     {
-      key: 'Blue Moon',
-      value: 'Blue Moon',
+      key: "Blue Moon",
+      value: "Blue Moon"
     },
     {
-      key: '805',
-      value: '805',
-    },
-  ]
+      key: "805",
+      value: "805"
+    }
+  ];
 
   renderSearchBox() {
     return (
-      <div style={{ width: "50%", marginRight: "auto", marginLeft: "auto", marginBottom: "40px" }}>
+      <div
+        style={{
+          width: "50%",
+          marginRight: "auto",
+          marginLeft: "auto",
+          marginBottom: "40px"
+        }}
+      >
         <ReactSearchBox
           placeholder="Search"
           data={this.data}
           callback={record => console.log(record)}
         />
       </div>
-    )
+    );
   }
 
   render() {
@@ -100,7 +117,7 @@ export class Home extends Component {
             marginLeft: "200px",
             marginRight: "200px",
             marginTop: "45px",
-            alignContent: "center",
+            alignContent: "center"
           }}
         >
           {this.renderLogo()}
@@ -116,7 +133,7 @@ const styles = {
   logoStyle: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "center",
+    alignItems: "center"
   },
   inRowStyle: {
     marginRight: "70px",
@@ -124,18 +141,18 @@ const styles = {
     display: "flex",
     flexDirection: "row",
     flex: 1,
-    alignItems: "top",
+    alignItems: "top"
   },
   inColumnStyle: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "center", 
-    flex: 1,
+    alignItems: "center",
+    flex: 1
   },
   inListStyle: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "right",
+    alignItems: "right"
   }
 };
 
