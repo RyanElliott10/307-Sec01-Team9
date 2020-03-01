a362dee9b4810a3a1bf6907564c1d104c1a63f58
Ryan Elliott <ryelliot@calpoly.edu>
2020-03-01 12:11:31 -0800

    Added data parsing for login/account creation, added Selenium for UI automation

---
+++ frontend/beerme/automation_tests/selenium_hello.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+from selenium import webdriver
+
+URL = 'http://localhost:3000/'
+
+DRIVER_PATH = f"{os.getcwd()}/geckodriver"
+driver = webdriver.Firefox(executable_path=DRIVER_PATH)
+driver.set_page_load_timeout(10)
+driver.get(URL)
+
+header_link_names = ["explore_header_link",
+                     "recommended_header_link", "account_account_entry_header_link"]
+header_links = [driver.find_element_by_class_name(
+    class_name) for class_name in header_link_names]
+
+for header in header_links:
+    time.sleep(2)
+    header.click()
+
+time.sleep(2)
+driver.quit()
