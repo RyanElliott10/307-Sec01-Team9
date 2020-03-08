b37f34b9de97a6f5ac920c20abae272126e097ae
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-03-08 13:18:58 -0700

    Revert "Fix login for frontend"
    This reverts commit 65b0723376fd1ae2b3c28fce2000b4c68ae7e950.

--- frontend/beerme/src/Controllers/UserController.js
+++ frontend/beerme/src/Controllers/UserController.js
@@ -19,22 +19,20 @@ class UserController {
       password: password
     };
 
-    const retValue = NetClient.post("https://localhost:44300/api/login", data).then(data => {
+    let retValue = null;
+    NetClient.post("http://httpbin.org/post", data).then(data => {
       if (data.Name) {
         this.firstName = data.Name.split(" ")[0];
         this.lastName = data.Name.split(" ")[1];
         this.email = data.Email;
         this.isBusiness = data.IsBusiness;
-        this.businessName = data.BusinessName;
-        return true;
+        this.businessName = data.businessName;
       }
-      return false;
     });
 
-    // retValue =
-    //   email === Constants.DUMMY_LOGIN_EMAIL &&
-    //   password === Constants.DUMMY_LOGIN_PASSWORD;
-    console.log("TEST:", retValue);
+    retValue =
+      email === Constants.DUMMY_LOGIN_EMAIL &&
+      password === Constants.DUMMY_LOGIN_PASSWORD;
     return retValue;
   }
 
@@ -57,18 +55,20 @@ class UserController {
       businessName: businessName
     };
 
-    const retValue = NetClient.post("https://localhost:44300/api/users", data).then(data => {
-      if (data.Name) {
+    let retValue = null;
+    NetClient.post("https://localhost:44300/api/users", data).then(data => {
+      if (data) {
         this.firstName = data.Name.split(" ")[0];
         this.lastName = data.Name.split(" ")[1];
         this.email = data.Email;
         this.isBusiness = data.IsBusiness;
-        this.businessName = data.BusinessName;
-        return true;
+        this.businessName = data.businessName;
       }
-      return false;
     });
-    
+
+    retValue =
+      email === Constants.DUMMY_LOGIN_EMAIL &&
+      password === Constants.DUMMY_LOGIN_PASSWORD;
     return retValue;
   }
 
