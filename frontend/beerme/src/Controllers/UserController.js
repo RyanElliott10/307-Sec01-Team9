9e4509764ffc8242156146513e1f8e5087073f5a
Ryan Elliott <ryelliot@calpoly.edu>
2020-03-10 22:11:44 -0700

    Remove dummy data

--- frontend/beerme/src/Controllers/UserController.js
+++ frontend/beerme/src/Controllers/UserController.js
@@ -14,7 +14,7 @@ class UserController {
   static currBeerId = 0;
   static userId = 0;
 
-  static isLoggedIn = true;
+  static isLoggedIn = false;
   static cachedBeers = [];
 
   static async login(email, password) {
