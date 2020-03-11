7a88cbb00d86c4a98ef24fe581225c722c3ce0c1
Ryan Elliott <ryelliot@calpoly.edu>
2020-03-10 17:53:27 -0700

    Added snapshot tests

--- frontend/beerme/src/Components/__tests__/Account.test.js
+++ frontend/beerme/src/Components/__tests__/Account.test.js
@@ -1,9 +1,31 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
 import Account from "../Account";
 
 it("renders correctly", () => {
   const tree = renderer.create(<Account />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it("renders correctly with state", () => {
+  const ref = shallow(<Account />).instance();
+  ref.setState({
+    addBeerData: {
+      name: "Corona",
+      styles: "Style-here",
+      color: "Pale"
+    }
+  });
+
+  const expectedState = {
+    addBeerData: {
+      name: "Corona",
+      styles: "Style-here",
+      color: "Pale"
+    }
+  };
+
+  expect(ref.state).toEqual(expectedState);
 });
--- frontend/beerme/src/Controllers/__tests__/UserController.test.js
+++ frontend/beerme/src/Controllers/__tests__/UserController.test.js
@@ -3,26 +3,44 @@ import UserController from "../UserController";
 // MARK: - login
 
 test("invalid email with a valid passowrd to login", async () => {
+  const originalError = console.error;
+  console.error = jest.fn();
+
   const success = await UserController.login("wrong-email", "password123");
   expect(success).toBeFalsy();
+  
+  console.error = originalError;
 });
 
 test("valid email with an invalid passowrd to login", async () => {
+  const originalError = console.error;
+  console.error = jest.fn();
+
   const success = await UserController.login("test@gmail.com", "Password123");
   expect(success).toBeFalsy();
+
+  console.error = originalError;
 });
 
 // MARK: - getCurrentuser
 
 test("getCurrentUser returns false when there is no logged in user", async () => {
+  const originalError = console.error;
+  console.error = jest.fn();
+
   await UserController.login("test@gmail.com", "password123");
   const currentUser = UserController.getCurrentUser();
   expect(currentUser).toBeFalsy();
+
+  console.error = originalError;
 });
 
 // MARK: - createAccount
 
 test("createAccount returns false when fed isBusiness=true and no business name", async () => {
+  const originalError = console.error;
+  console.error = jest.fn();
+
   const success = await UserController.createAccount(
     "First",
     "Last",
@@ -32,9 +50,14 @@ test("createAccount returns false when fed isBusiness=true and no business name"
     null
   );
   expect(success).toBeFalsy();
+
+  console.error = originalError;
 });
 
 test("createAccount returns true when fed isBusiness=true and valid business name", async () => {
+  const originalError = console.error;
+  console.error = jest.fn();
+
   const success = await UserController.createAccount(
     "First",
     "Last",
@@ -44,9 +67,14 @@ test("createAccount returns true when fed isBusiness=true and valid business nam
     "businessName"
   );
   expect(success).toBeFalsy();
+
+  console.error = originalError;
 });
 
 test("createAccount returns true when fed not a business", async () => {
+  const originalError = console.error;
+  console.error = jest.fn();
+
   const success = await UserController.createAccount(
     "First",
     "Last",
@@ -56,6 +84,8 @@ test("createAccount returns true when fed not a business", async () => {
     null
   );
   expect(success).toBeFalsy();
+
+  console.error = originalError;
 });
 
 // MARK: - getCurrentUserObject
@@ -85,6 +115,11 @@ test("getCurrentUserObject returns an object with the correct fields when a user
 // MARK: - fetchAllBeers
 
 test("fetchAllBeers should return a list with all the beers", async () => {
+  const originalError = console.error;
+  console.error = jest.fn();
+
   await UserController.fetchAllBeers();
   expect(UserController.cachedBeers).toBeTruthy();
-})
+
+  console.error = originalError;
+});
