bcc9485792dc76ca187476cc084068a1ba2ed94f
Ryan Elliott <ryelliot@calpoly.edu>
2020-03-10 18:04:15 -0700

    Fix broken snapshot tests, add <Router>

--- frontend/beerme/src/Components/__tests__/LockedRecommended.test.js
+++ frontend/beerme/src/Components/__tests__/LockedRecommended.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { MemoryRouter as Router } from "react-router-dom";
 
 import LockedRecommended from "../LockedRecommended";
 
 it("renders correctly", () => {
-  const tree = renderer.create(<LockedRecommended />).toJSON();
+  const tree = renderer
+    .create(
+      <Router>
+        <LockedRecommended />
+      </Router>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- frontend/beerme/src/Components/__tests__/Recommended.test.js
+++ frontend/beerme/src/Components/__tests__/Recommended.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { MemoryRouter as Router } from "react-router-dom";
 
 import Recommended from "../Recommended";
 
 it("renders correctly", () => {
-  const tree = renderer.create(<Recommended />).toJSON();
+  const tree = renderer
+    .create(
+      <Router>
+        <Recommended />
+      </Router>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
