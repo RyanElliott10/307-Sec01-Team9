a48fccb41f9ece4694dce2658f96264f8a98e132
Ryan Elliott <ryelliot@calpoly.edu>
2020-03-10 20:42:32 -0700

    Fix broken tests

--- frontend/beerme/src/Components/__tests__/Account.test.js
+++ frontend/beerme/src/Components/__tests__/Account.test.js
@@ -1,16 +1,27 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
+import { BrowserRouter } from "react-router-dom";
 
 import Account from "../Account";
 
 it("renders correctly", () => {
-  const tree = renderer.create(<Account />).toJSON();
+  const tree = renderer
+    .create(
+      <BrowserRouter>
+        <Account />
+      </BrowserRouter>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 it("renders correctly with state", () => {
-  const ref = shallow(<Account />).instance();
+  const ref = shallow(
+    <BrowserRouter>
+      <Account />
+    </BrowserRouter>
+  ).instance();
   ref.setState({
     addBeerData: {
       name: "Corona",
