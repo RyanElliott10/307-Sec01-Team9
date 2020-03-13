6eb00a3d98158245c39fb43f24302109d83e4dc7
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-03-12 21:08:13 -0700

    Fixed last automated test

--- frontend/beerme/automation_tests/AutomationTest5.py
+++ frontend/beerme/automation_tests/AutomationTest5.py
@@ -17,3 +17,10 @@ time.sleep(1)
 
 BeerMeDriver.getExploredStyles(3,2,2)
 time.sleep(2)
+
+exploreResults = driver.find_elements_by_id("clickable-beer")
+
+if(len(exploreResults)>0):
+    print("Test Successful")
+else:
+    print("Test Unsuccessful")
